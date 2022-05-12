99b001cfcd8bd7a9b7329dc2ab7fcbf26f6f398f
nurrizkyimn <nurrizkyimanidev@gmail.com>
2022-05-12 18:12:21 +0700

    add connection gcp sql add photo

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+from google.cloud import storage
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Integer, Column, String, ForeignKey, select, inspect
+from sqlalchemy.orm import declarative_base, Session
 
 app = FastAPI()
 
 # create a connection between fastapi and gcp bucket
+engine = create_engine(
+    "postgresql://postgres:Denhaag898!@34.132.87.247:5432/postgres", echo=True, future=True)
+
+# Declare the base
+
+Base = declarative_base()
+
+
+class Photo(Base):
+    __tablename__ = "photosdb"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    url = Column(String)
+    vote = Column(Integer)
+
+    def as_dict(self):
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+
+
+# create the table
+Base.metadata.create_all(engine)
 
 # main route
 
@@ -36,15 +60,31 @@ async def user():
 
 @app.get("/upload_photo")
 async def upload_photo():
+    """ Upload photo to the gcp bucket """
+    bucket_name = "photoalbumsppl"
+    source_file_name = "test_img_aot.jpg"
+    destination_blob_name = "photos/{}.jpg".format(source_file_name)
 
-    # take the data from the body
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
 
-    # post into google cloud
+    # blob.upload_from_filename(source_file_name)
 
-    # return a appropriate response
+    res = "File {} uploaded to {}.".format(
+        source_file_name, destination_blob_name)
 
-    # add the id of the photo into sql database
-    return {"message": "Upload Picture"}
+    # declare_model
+
+    photo_1 = Photo(
+        name="test", url="https://storage.googleapis.com/photoalbumsppl/photos/test_img_aot.jpg")
+
+    # post the photo with user data to the gcp sql database
+    with Session(engine) as session:
+        session.add(photo_1)
+        session.commit()
+
+    return {"message": res}
 
 # upvote photo route
 
@@ -87,7 +127,13 @@ async def top_10_photo_week():
 @app.get('/all_photos')
 async def all_photos():
 
-    return {"message": "All Photos"}
+    with Session(engine) as session:
+        stmt = select(Photo)
+        photos_arr = []
+        for photo in session.scalars(stmt):
+            photos_arr.append(photo.as_dict())
+
+    return {"message": photos_arr}
 
 
 # resize the photo with the single click
