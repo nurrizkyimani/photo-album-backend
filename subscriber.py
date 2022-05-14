db7e299c337282124a19fd7763da5761739784db
nurrizkyimn <nurrizkyimanidev@gmail.com>
2022-05-14 18:31:40 +0700

    add comment and cancel subs in errorthrow

--- subscriber.py
+++ subscriber.py
@@ -15,9 +15,11 @@ arr_list = []
 def callback(message: pubsub_v1.subscriber.message.Message):
     # print(f"Received {message.data!r}.")
 
+    # get data and decode it from bytestring
     json_res = message.data
     res_json_decode = json.loads(json_res)
 
+    # add to arr list as the result
     arr_list.append(res_json_decode)
     print(arr_list)
 
@@ -34,6 +36,7 @@ while True:
 
     print(f"Listening for messages on {subscription_path}..\n")
 
+    # subcribing and pass the data into the callback
     streaming_pull_future = subscriber.subscribe(
         subscription_path,
         callback=callback
@@ -48,5 +51,6 @@ while True:
         except TimeoutError:
             # Trigger the shutdown.  # Block until the shutdown is complete.
             streaming_pull_future.cancel()
+            streaming_pull_future.result()
 
     time.sleep(3)
