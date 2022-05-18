4faa62e32efc8584393230d2fe7a1c58864ee0e2
nurrizkyimn <nurrizkyimanidev@gmail.com>
2022-05-18 13:12:16 +0700

    add auth bearer

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 
 
 class Photo(BaseModel):
@@ -9,11 +9,6 @@ class Photo(BaseModel):
     thumbnail_url: str
     square_url: str
     userid: str
-
-
-class User(BaseModel):
-    username: str
-    password: str
 
 
 class Albums(BaseModel):
@@ -26,3 +21,46 @@ class Summary(BaseModel):
     top_weekly: List[str] = []
     top_monthly: List[str] = []
     top_daily: List[str] = []
+
+
+class UserSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "Abdulazeez Abdulazeez Adeshina",
+                "email": "abdulazeez@x.com",
+                "password": "weakpassword"
+            }
+        }
+
+
+class UserSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "Abdulazeez Abdulazeez Adeshina",
+                "email": "abdulazeez@x.com",
+                "password": "weakpassword"
+            }
+        }
+
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "abdulazeez@x.com",
+                "password": "weakpassword"
+            }
+        }
